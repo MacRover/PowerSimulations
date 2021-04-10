205124f483f36a31fc336810b26121d133f02d49
Matt Lemcke <53319315+Matt-Lemcke@users.noreply.github.com>
2021-04-10 19:43:14 -0400

    Added files for part 7

---
+++ SimulationTutorial/Part7/editvars.m
@@ -0,0 +1,26 @@
+load('rovervars');
+
+mass = 1;
+radius = 0.1;
+stepTime = 2;
+torqueF = 0.05;
+
+massRover = 50;
+area = 0.6;
+
+windSpeed = 0;
+angle = 0;
+
+inductance = 12e-10;
+stallTorque = 0.43;
+noloadSpeed = 13180;
+ratedVolt = 12;
+noloadCurrent = 1.8;
+rotorInertia = 200;
+
+pwmVolt = 5;
+
+dutyInitial = 0;
+dutyFinal = 1;
+
+save('rovervars');
---
+++ SimulationTutorial/Part7/runsim.m
@@ -0,0 +1,4 @@
+clear
+clc
+load('rovervars.mat')
+run('PowerSim.mlapp')
